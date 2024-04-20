d7fb27357204a2e363be862322bc4c32544a6a03
Niraj Phutane <nirajphutane.np@gmail.com>
2024-04-20 10:36:21 +0530

    Initialize Android Project

---
+++ app/src/main/java/com/np/dagger2/SampleActivity.kt
@@ -0,0 +1,37 @@
+package com.np.dagger2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.np.dagger2.ui.theme.Dagger2Theme
+
+class SampleActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { View() }
+    }
+
+    @Composable
+    private fun View() {
+        Dagger2Theme {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                Box(contentAlignment = Alignment.Center) {
+                    Text(
+                        text = "Dagger 2 Practice"
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/np/dagger2/core/AppLog.kt
@@ -0,0 +1,5 @@
+package com.np.dagger2.core
+
+import android.util.Log
+
+fun appLog(message: String) = Log.d("App Dagger2", message)
